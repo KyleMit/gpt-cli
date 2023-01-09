2030e91233055ad1c44be206d7e00973c689395e
kylemit <Kylemit@gmail.com>
2023-01-08 20:26:29 -0500

    uninstall openai client

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/** @type {import("./node_modules/openai/dist/api").CreateCompletionRequest} */
+/** @type {import("./types").CreateCompletionRequest} */
 export const config = {
     model: "text-davinci-003",
     temperature: 0.6,
